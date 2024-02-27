4ea4934ac95f52f9153d9a12585c26773322bfa5
mehmethalis <mehmethaliscicek@gmail.com>
2024-02-27 21:44:42 +0300

    feat(alert): added props and variants

--- src/components/feedback/Alert/types.ts
+++ src/components/feedback/Alert/types.ts
@@ -1,4 +1,30 @@
+import { Color, Size } from '@/types'
+import { ReactNode } from 'react'
+
 type AlertProps = {
+  /**
+   * Alert bileşinin contentinde render edilir.
+   */
+  children?: ReactNode
+  /**
+   * Alert bileşini class namelerini temsil eden proptur.
+   */
+  className?: string
+  /**
+   * Alert bileşini rengini temsil eden proptur.
+   *  @default primary
+   */
+  color?: Color
+  /**
+   * Eğer false geçilir ise, Alert bileşinine border ekler.
+   *  @default true
+   */
+  disableBordered?: boolean
+  /**
+   * Alert bileşini boyutunu temsil eden proptur.
+   *  @default md
+   */
+  size?: Size
   /**
    * Alert bileşeni id değerini temsil eden proptur.
    */
